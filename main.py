14bafe2cc007a0afef0d6d5a8048882a30a55d22
Anthony Zampino <antz@anthonys-air.home>
2024-03-05 21:35:54 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from utility import *
+from shopping.more_shopping import shopping_cart
+from random import shuffle
+import sys
+
+# if __name__ == '__main__':
+#     print(multiply(2, 5))
+#     print(divide(20, 5))
+#
+#     print(shopping_cart.buy('apple'))
+#
+#     print(max([1, 2, 3]))
+#
+#     print(__name__)
+
+li = [1, 2, 3, 4, 5, 6, 7]
+shuffle(li)
+print(li)
+
